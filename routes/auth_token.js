763c64cffd7162af304f6b2ad824d86d70aa5f4a
mateo <mateo.lamas@udc.es>
2023-08-13 21:34:35 +0200

    Validaciones con AJV y JSON Schema

--- routes/auth_token.js
+++ routes/auth_token.js
@@ -2,10 +2,13 @@ import {Router} from "express";
 import authByEmailPwd from "../helpers/authByEmailPwd.js"
 import { SignJWT, jwtVerify } from "jose";
 import { USERS_BBDD } from "../bbdd.js";
+import validateLoginDTO from "../dto/validate_login_dto.js"
 
 const authTokenRouter = Router();
 
-authTokenRouter.post("/login", async (req,res) =>  {
+
+
+authTokenRouter.post("/login", validateLoginDTO, async (req,res) =>  {
     const {email, password} = req.body;
 
     if(!email || !password) return res.sendStatus(400);
